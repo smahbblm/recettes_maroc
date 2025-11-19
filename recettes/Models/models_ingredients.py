2947e9a672beae8c548ffd7b3d9fa7e4e22639e5
smahan blm <boulmaneismahane@gmail.com>
2025-11-19 17:07:25 +0100

    debut de code

---
+++ recettes/Models/models_ingredients.py
@@ -0,0 +1,8 @@
+# la définition de la classe Ingriends :
+
+from django.db import models
+class Ingredinets(models.Models):
+
+    nom                        = models.TextField()
+    Categorie                  =
+    Disponibitile_saisonnière  = 
