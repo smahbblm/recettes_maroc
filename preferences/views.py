b14dd1177e1e05068f397d8cfd3448c11bdd4e7d
smahan blm <boulmaneismahane@gmail.com>
2025-11-26 15:01:07 +0100

    test

--- preferences/views.py
+++ preferences/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Categorie, Region, Ingredient, PreferenceUtilisateur
+from .serializers import CategorieSerializer, RegionSerializer, IngredientSerializer, PreferenceSerializer
 
-# Create your views here.
+class CategorieListView(generics.ListAPIView):
+    queryset = Categorie.objects.all()
+    serializer_class = CategorieSerializer
+
+class RegionListView(generics.ListAPIView):
+    queryset = Region.objects.all()
+    serializer_class = RegionSerializer
+
+class IngredientListView(generics.ListAPIView):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+
+class PreferenceView(generics.CreateAPIView):
+    serializer_class = PreferenceSerializer
