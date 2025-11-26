0b499d0e11b49dc39e67e9180afb6da311a035ec
Naima <khayechounaimaa@gmail.com>
2025-11-26 17:15:34 +0100

    filteering_part

--- api/views.py
+++ api/views.py
@@ -1,1 +1,27 @@
 #  Les Vues API (reçoit la requête et appelle le moteur).
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from .services.recommender_engine import filter_by_content
+from .services.nlp_engine import Nlp
+
+nlp_engine = Nlp(csv_path="data/datafr_cleaned.csv")
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def recommandations(request):
+    user = request.user
+    query = request.GET.get('query', '').strip()
+
+    if query == "":
+        return Response({"error": "Paramètre 'query' manquant."}, status=400)
+
+    nlp_results = nlp_engine.Search_query(query, top_k=20)
+    final_results = filter_by_content(user, nlp_results)
+
+    return Response({
+        "query": query,
+        "count": len(final_results),
+        "results": final_results,
+    })
--- config_recettes/settings.py
+++ config_recettes/settings.py
@@ -28,30 +28,22 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
-# Application definition
-
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-     #application == composntes
-]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-   
-    'corsheaders.middleware.CorsMiddleware',  # ← NOUVEAU (en premier)
-    
+
+    # IMPORTANT : CORS AVANT CommonMiddleware
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+
 
 ROOT_URLCONF = 'config_recettes.urls'
 
@@ -154,6 +146,16 @@ INSTALLED_APPS = [
     'account',
     'interactions',
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
 ]
